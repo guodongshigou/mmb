ffe0cb9c3334a5f5934c0987fd4866373cbb3864
yuan lin <250679545@qq.com>
2018-08-18 02:37:41 +0800

    提交错了   哭...

---
+++ js/lowPrice.js
@@ -0,0 +1,24 @@
+$(function(){
+    document.querySelector('html').style.fontSize = screen.width / 10 + 'px';
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
