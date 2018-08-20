f733131ac4056cb846dc50e476b1e4253f067e54
y <499064493@qq.com>
2018-08-20 16:40:46 +0800

    国内折扣

---
+++ js/zhekou.js
@@ -0,0 +1,21 @@
+$(function () {
+    function getinlanddiscount() {
+        $.ajax({
+            url: "http://mmb.ittun.com/api/getinlanddiscount",
+            type: "GET",
+            success: function (res) {
+                console.log(res);
+                var htmlStr = template("getland", { list: res.result })
+                $(".main").html(htmlStr);
+            }
+
+        })
+    }
+    getinlanddiscount();
+
+    $('.main').on('click', '.chanping', function () {
+        var id = $(this).data('id');
+        location.href = "xiangqing.html?productId="+id;
+    })
+
+});
