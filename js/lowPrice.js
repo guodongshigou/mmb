d901bc6cf69766411283baf74f1d19f91882fd33
yuan lin <250679545@qq.com>
2018-08-20 15:15:43 +0800

    修改了commom.js

--- js/lowPrice.js
+++ js/lowPrice.js
@@ -1,16 +1,9 @@
 $(function () {
-    document.querySelector('html').style.fontSize = screen.width / 10 + 'px';
-    mui('.mui-scroll-wrapper').scroll({
-        deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-    });
-
     $.ajax({
         url: 'http://mmb.ittun.com/api/getbaicaijiatitle',
         type: 'get',
         dataType: 'json',
         success: function (res) {
-            console.log(res);
-
             var html = template('topNavTpl', {
                 list: res.result
             });
@@ -34,7 +27,6 @@ $(function () {
             type: 'get',
             dataType: 'json',
             success: function (res) {
-                console.log(res);
                 var html = template('productTpl', {
                     list: res.result
                 });
@@ -45,16 +37,9 @@ $(function () {
 
 
 
-    $('.fixed, .top').click(function () {        
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 500);
-        return false;
-    });
+    
 
-    $('.back').click(function(){
-        history.back();
-    });
+    
 
 
 
